6ed6316ca47102b511a2a76f0ab95adbd3d4586d
dfsdanimaulana <danimaulana9f@gmail.com>
2023-09-19 21:52:06 +0700

    docs(git): how to add scope in git commit message

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,4 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
+    helpUrl: `Invalid commit message format. Please check the commit message format guidelines in the \x1b[34m[docs/commit-msg.md]\x1b[0m file.`,
 }
